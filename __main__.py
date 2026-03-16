aff9e69bbe1f841a5e55b3bc868b73b931f562ac
mariamcherif-creator <mariam.cherif@etu.uae.ac.ma>
2026-03-16 23:57:52 +0000

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -1,10 +1,22 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2009-2020 the sqlparse authors and contributors
+# <see AUTHORS file>
+#
+# This module is part of python-sqlparse and is released under
+# the BSD License: https://opensource.org/licenses/BSD-3-Clause
+
+"""Entrypoint module for `python -m sqlparse`.
+
+Why does this file exist, and why __main__? For more info, read:
+- https://www.python.org/dev/peps/pep-0338/
+- https://docs.python.org/2/using/cmdline.html#cmdoption-m
+- https://docs.python.org/3/using/cmdline.html#cmdoption-m
 """
-Invokes django-admin when the django module is run as a script.
 
-Example: python -m django check
-"""
+import sys
 
-from django.core import management
+from sqlparse.cli import main
 
-if __name__ == "__main__":
-    management.execute_from_command_line()
+if __name__ == '__main__':
+    sys.exit(main())
